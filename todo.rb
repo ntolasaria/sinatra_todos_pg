ae71c886f6280163ba6aeccfc84339411f26c344
Nimish Tolasaria <ntolasaria@icloud.com>
2023-01-18 22:16:48 +0530

    DatabasePersistence updated and running with PostgreSQL

--- todo.rb
+++ todo.rb
@@ -1,15 +1,18 @@
 require "sinatra"
-require "sinatra/reloader" if development?
 require "sinatra/content_for"
 require "tilt/erubis"
+
+require_relative "database_persistence"
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
+  set :erb, :escape_html => true
 end
 
-configure do
-  set :erb, :escape_html => true
+configure(:development) do
+  require "sinatra/reloader"
+  also_reload "database_persistence.rb"
 end
 
 helpers do
@@ -44,67 +47,6 @@ helpers do
   end
 end
 
-class SessionPersistence
-
-  def initialize(session)
-    @session = session
-    @session[:lists] ||= []
-  end
-
-  def find_list(id)
-    @session[:lists].find { |list| list[:id] == id }
-  end
-
-  def all_lists
-    @session[:lists]
-  end
-
-  def create_new_list(list_name)
-    id = next_element_id(@session[:lists])
-    @session[:lists] << { id: id, name: list_name, todos: [] }
-  end
-
-  def delete_list(id)
-    @session[:lists].reject! { |list| list[:id] == id }
-  end
-
-  def update_list_name(id, new_name)
-    list = find_list(id)
-    list[:name] = new_name
-  end
-
-  def create_new_todo(list_id, todo_name)
-    list = find_list(list_id)
-    id = next_element_id(list[:todos])
-    list[:todos] << {id: id, name: todo_name, completed: false}
-  end
-
-  def delete_todo_from_list(list_id, todo_id)
-    list = find_list(list_id)
-    list[:todos].reject! { |todo| todo[:id] == todo_id }
-  end
-
-  def update_todo_status(list_id, todo_id, new_status)
-    list = find_list(list_id)
-    todo = list[:todos].find { |todo| todo[:id] == todo_id }
-    todo[:completed] = new_status
-  end
-
-  def mark_all_todos_as_completed(list_id)
-    list = find_list(list_id)
-    list[:todos].each do |todo|
-      todo[:completed] = true
-    end
-  end
-
-  private
-
-  def next_element_id(elements)
-    max = elements.map { |element| element[:id] }.max || 0
-    max + 1
-  end
-end
-
 def load_list(id)
   list = @storage.find_list(id)
   return list if list
@@ -113,7 +55,7 @@ def load_list(id)
 end
 
 before do
-  @storage = SessionPersistence.new(session)
+  @storage = DatabasePersistence.new(logger)
 end
 
 get "/" do
